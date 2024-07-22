b2bd1e20b73196da2b972cd41aa25c9f7fb5c46f
Dow-Dodds <100536374+Dow-Dodds@users.noreply.github.com>
2024-07-22 22:01:39 +1200

    add existing files to git

---
+++ components/navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav className='flexBetween navbar'>
+        <div className='flex-1 flexStart gal-10'>
+            <link href="/">
+
+            </link>
+        </div>
+
+    </nav>
+  )
+}
+
+export default navbar
